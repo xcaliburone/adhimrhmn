602a024fec736f75f05b1fdb9d334e2826b20d5c
adhimrahman <adhimrahaman@gmail.com>
2023-08-09 00:55:09 +0800

    update image icon

---
+++ tugas-ci22-react/src/components/ContactCard/index.jsx
@@ -0,0 +1,11 @@
+import Styles from './ContactCard.module.css'
+
+export default function ContactCard() {
+    return(
+        <>
+            <div className={Styles.container}>
+                Contact Card
+            </div>
+        </>
+    );
+}
---
+++ tugas-ci22-react/src/components/Form/index.jsx
@@ -0,0 +1,12 @@
+import Styles from './Form.module.css'
+
+export default function Form() {
+
+    return(
+        <>
+            <div className={Styles.container}>
+                Form
+            </div>
+        </>
+    );
+}
