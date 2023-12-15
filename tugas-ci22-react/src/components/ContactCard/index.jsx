200dcec29f40f1a3cefd42242cf0a3520dfac07b
adhimrahman <adhimrahaman@gmail.com>
2023-12-15 22:00:15 +0800

    delete

--- tugas-ci22-react/src/components/ContactCard/index.jsx
+++ tugas-ci22-react/src/components/ContactCard/index.jsx
@@ -17,6 +17,7 @@ export default function ContactCard() {
                 <Card />
                 <Card />
                 <Card />
+                <Card />
             </div>
         </div>
     );
